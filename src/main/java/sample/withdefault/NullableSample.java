a342c4695bdaa0bd0ea3ca06973dcc68f9e94e66
Christian Asselmeyer <christian.asselmeyer@faktorzehn.at>
2018-11-27 12:53:16 +0100

    Initial commit

---
+++ src/main/java/sample/withdefault/NullableSample.java
@@ -0,0 +1,12 @@
+package sample.withdefault;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+public class NullableSample {
+
+	@Nullable
+	public Object maybeNull() {
+		return null;
+	}
+	
+}
---
+++ src/main/java/sample/withoutdefault/NullableSample.java
@@ -0,0 +1,12 @@
+package sample.withoutdefault;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+public class NullableSample {
+
+	@Nullable
+	public Object maybeNull() {
+		return null;
+	}
+	
+}
